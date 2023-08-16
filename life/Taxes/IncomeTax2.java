1cf883dd9fd53eded64ac7c5ab2a85550c0f737c
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-08-16 17:25:51 -0400

    Add files via upload

---
+++ life/Taxes/IncomeTax2.java
@@ -0,0 +1,59 @@
+//Source: https://www.irs.gov/newsroom/irs-provides-tax-inflation-adjustments-for-tax-year-2023
+import java.util.Scanner;
+
+class IncomeTax2 {
+   public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Hello! What is your name? ");
+      String name = keyboard.nextLine();
+      System.out.print("Please state your gross income: ");
+      int grossIncome = keyboard.nextInt();
+      System.out.print("Please state the cumulative value of the untaxed deductions: ");
+      int deductions = keyboard.nextInt();
+      int taxIncome = calculateTaxableIncome(deductions,grossIncome);
+      float taxValue = incomeTaxCalc((float)taxIncome);
+      System.out.println(name + ",");
+      System.out.printf("\tYou must pay $%,.2f in income tax to the federal government.", taxValue);
+   }
+   
+   public static float incomeTaxCalc(float taxIncome) {
+      float remainder;
+      float incomeTaxValue = 0;
+      while (taxIncome > 0) {
+         if (taxIncome > 231250) {
+            remainder = taxIncome - 231250;
+            incomeTaxValue += remainder * 0.35;
+            taxIncome = 231250;
+         } else if (taxIncome > 182100) {
+            remainder = taxIncome - 182100;
+            incomeTaxValue += remainder * 0.32;
+            taxIncome = 182100;
+         } else if (taxIncome > 95375) {
+            remainder = taxIncome - 95375;
+            incomeTaxValue += remainder * 0.24;
+            taxIncome = 95375;
+         } else if (taxIncome > 44725) {
+            remainder = taxIncome - 44725;
+            incomeTaxValue += remainder * 0.22;
+            taxIncome = 44725;
+         } else if (taxIncome > 11000) {
+            remainder = taxIncome - 11000;
+            incomeTaxValue += remainder * 0.12;
+            taxIncome = 11000;
+         } else {
+            incomeTaxValue += taxIncome * 0.1;
+            taxIncome = 0;
+         }
+      }
+      
+      return incomeTaxValue;
+   }
+   
+   public static int calculateTaxableIncome (int deduction, int salary) {
+      if (deduction < 13850) {
+         return salary - 13850;
+      } else {
+         return salary - deduction;
+      }
+   }
+}
