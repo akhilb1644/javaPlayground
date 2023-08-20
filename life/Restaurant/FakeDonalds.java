49b65a8b9c9fe6552d9d18be3a10bd78ffd918ef
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-08-20 19:33:26 -0400

    Add files via upload

--- life/Restaurant/FakeDonalds.java
+++ life/Restaurant/FakeDonalds.java
@@ -8,42 +8,53 @@ class FakeDonalds {
       Scanner keyboard = new Scanner(System.in);
       int j;
       int choice;
+      float tip;
       String[] menuItems = {"Omlette","Chicken Nuggets","French Fries","Sandwich","Chips"};
       double[] prices = {3.25,5.67,5.90,9.87,2.14};
       System.out.print("Hello! What is your name? ");
       String name = keyboard.nextLine();
       int again = 1;
       String loop;
-      while (true) {
-         System.out.println("The following are options on the menu, please pick a number to order.");
-         for (int i=0;i<5;i++) {
-            j = i + 1;
-            System.out.println(j +". "+ menuItems[i]);
-         }
-         System.out.print("Pick a number from above: ");
-         choice = keyboard.nextInt();
+      do {
+         do {
+            System.out.println("The following are options on the menu, please pick a number to order.");
+            for (int i=0;i<5;i++) {
+               j = i + 1;
+               System.out.println(j +". "+ menuItems[i]);
+            }
+            System.out.print("Pick a number from above: ");
+            choice = keyboard.nextInt();
+            if (choice < 1 || choice > 5) {
+               System.out.println("\nPlease enter a valid choice\n");
+            }
+         } while (choice < 1 || choice > 5);
          System.out.print("How many units of " + menuItems[choice] + " would you like to order.");
          int units = keyboard.nextInt();
          price += (double)prices[choice] * units;
-         System.out.printf("The current price of your order is $%.2f %n ", price);
-         System.out.println("Just a quick question now");
+         System.out.printf("The current price of your order is $%,.2f \n", price);
          // The important part of the loop is below
-         System.out.print("Would you like to order more(Y/N)? ");
-         loop = keyboard.nextLine();
-         String uLoop = loop.toUpperCase();
-         if (uLoop != "Y") {
-            break;
+         do {
+            System.out.println("Would you like to order more(Y/N)? ");
+            loop = keyboard.nextLine();
+            loop.toUpperCase();
+            if (!(loop.equals("Y") || loop.equals("N"))) {
+               System.out.println("\nPlease enter a valid choice, which is Y or N\n");
+            }
+         } while (!(loop.equals("Y") || loop.equals("N")));
+      } while (loop.equals("Y"));
+      do {
+         System.out.print("Would you like to add a tip(enter percent in decimal form, if you don't want to, enter zero).");
+         tip = keyboard.nextFloat();
+         if (tip < 0) {
+            System.out.println("PLEASE ENTER A TIP IN VALID DECIMAL FORM. NO NEGATIVE NUMBERS.");
          }
-         System.out.println("-----------");
-      }
-      System.out.print("Would you like to add a tip(enter percent in decimal form, if you don't want to, enter zero).");
-      float tip = keyboard.nextFloat();
-      System.out.printf("Subtotal: $%.2f %n",price);
+      } while (tip < 0);
+      System.out.printf("Subtotal: $%,.2f\n",price);
       float tax = salesTaxRate * price;
-      System.out.printf("Sales Tax: $%.2f %n",tax);
+      System.out.printf("Sales Tax: $%,.2f\n",tax);
       price += tax;
       price += price * tip;
-      System.out.printf("Tip amount: $%.2f %n",price*tip);
-      System.out.printf("Total: $%.2f %n",price); 
+      System.out.printf("Tip amount: $%,.2f\n",price*tip);
+      System.out.printf("Total: $%,.2f\n",price); 
    }
 }
