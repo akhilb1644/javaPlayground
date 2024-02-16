64a55f45c712a32b77a3ad1922f68c6e6fd306ef
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-02-16 13:03:14 -0500

    Add files via upload

--- Generics/Main.java
+++ Generics/Main.java
@@ -5,7 +5,10 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		// With N = number
+		/* UsefulNumber<String> i = new UsefulNumber<>("hi"); 
+			- The above line would be an error due to bounded types
+		*/
+
 		UsefulNumber<Double> u = new UsefulNumber<>(3.14);
 		System.out.println(u.getHashCode());
 
@@ -26,5 +29,9 @@ word is a raw type
 		word.printInfo();
 		System.out.println("");
 		System.out.println(formalPrint(word)); // No need to define K and V, they are inferred from "word"
+
+		short y = 7634;
+		LessMemoryNumber<Short> lev = new LessMemoryNumber<Short>(y);
+		System.out.println(lev.giveTheHash());
 	}
 }
--- Generics/UsefulNumber.java
+++ Generics/UsefulNumber.java
@@ -1,8 +1,12 @@
-public class UsefulNumber<N> {
+public class UsefulNumber<N extends Number> { // bounded type parameters
 	private N number;
 
 	public UsefulNumber(N number) {
 		this.number = number;
+	}
+
+	public void printInfo() {
+		System.out.println(number);
 	}
 
 	public int getHashCode() {
