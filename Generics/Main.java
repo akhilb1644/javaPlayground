182a6d3b4ee08c7a6111c1231fd6f883fef6fc8e
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-02-17 22:50:50 -0500

    Add files via upload

--- Generics/Main.java
+++ Generics/Main.java
@@ -4,6 +4,10 @@ public class Main {
 		return "Entry: " + w1.getEntry() + "\nDescription: " + w1.getDescription();
 	}
 
+	public static void printDict(DictionaryWord<Object,Object> u) {
+		System.out.println(u);
+	}
+
 	public static void main(String[] args) {
 		/* UsefulNumber<String> i = new UsefulNumber<>("hi"); 
 			- The above line would be an error due to bounded types
@@ -31,7 +35,14 @@ word is a raw type
 		System.out.println(formalPrint(word)); // No need to define K and V, they are inferred from "word"
 
 		short y = 7634;
-		LessMemoryNumber<Short> lev = new LessMemoryNumber<Short>(y);
+		LessMemoryNumber<Number> lev = new LessMemoryNumber<Number>(y); // can use the class that it "extends"(Number)
 		System.out.println(lev.giveTheHash());
+
+		// printDict(coffee);
+		// DictionaryWord<String,String> isn't derived from DictionaryWord<Object,Object>
+		
+		Order<Integer> o = new LocalOrder<Integer>(3,6,"Denmark","Estonia");
+
+		System.out.println(o.getValue());
 	}
 }
