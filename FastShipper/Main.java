ebeeb72acb757cccf0305090e51cace52c97aa36
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-02-20 19:27:59 -0500

    Add files via upload

--- FastShipper/Main.java
+++ FastShipper/Main.java
@@ -1,9 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		// Checking to make sure the programs compile
+		Scanner scnr = new Scanner(System.in);
+		int numPlayers;
+
+		System.out.print("Hello, please enter how many players you would like to play? ");
+		numPlayers = scnr.nextInt();
+		Player[] players = new Player[numPlayers];
+		scnr.nextLine();
 		
-		Player p = new Player();
+		for (int i = 0;i < numPlayers;i++) {
+			System.out.print("What is the name for the player? ");
+			players[i] = new Player(scnr.nextLine());
+			System.out.println(players[i]);
+		}
 	}
 }
