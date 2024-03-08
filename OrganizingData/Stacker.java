220033b47e55d5a1e9c4665bbb963ad15165d4a1
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-03-08 18:55:57 -0500

    Add files via upload

---
+++ OrganizingData/Stacker.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Stacker {
+	public static void main(String[] args) {
+		Stack<String> states = new Stack<>();
+		
+		states.push("Alabama");
+		states.push("Alaska");
+		states.push("Arizona");
+
+		System.out.println(states);
+
+		System.out.println(states.peek()); // peek at top element
+
+		states.pop(); // removes top element
+
+		System.out.println(states);
+
+		System.out.println(states.search("Alabama"));
+		System.out.println(states.search("Arkansas"));
+	}
+}
