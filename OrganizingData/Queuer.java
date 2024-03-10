6daadc92ee6ff8d750805c3503ece613c278ae80
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-03-10 18:59:58 -0400

    Add files via upload

---
+++ OrganizingData/Queuer.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+@SuppressWarnings("removal")
+
+public class Queuer {
+	public static void main(String[] args) {
+		Queue<String> countries = new PriorityQueue<>();
+
+		countries.add("France");
+
+		System.out.println(countries);
+
+		countries.offer("Sweden");
+
+		System.out.println("Without violating the capacity restriction: " + countries);
+
+		countries.remove("France");
+
+		System.out.println(countries);
+	}
+}
