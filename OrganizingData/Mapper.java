bbf7773b2e1bbcec3fc99616f6bc8ab6728740e4
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-03-10 14:17:33 -0400

    Add files via upload

--- OrganizingData/Mapper.java
+++ OrganizingData/Mapper.java
@@ -71,42 +71,9 @@ These are unordered, so they won't show up in the below order
 		System.out.println(birthYears.getOrDefault("Alexei Rykov",1881) + "\n");
 
 		System.out.println("\"birthYears\" after getOrDefault method: " + birthYears + "\n");
+
+		birthYears.putIfAbsent("Alexei Rykov",1881);
+
+		System.out.println("\"birthYears\" after putIfAbsent method: " + birthYears + "\n");
 	}
 }
-
-/*
-Output:
-{Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Vyacheslav Molotov=1890, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1780}
-
-{Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Vyacheslav Molotov=1890, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1870}
-
-{Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1870}
-
-{}
-
-{Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1870}
-
-Is Vyacheslav Molotov there? false
-
-Was someone born in 1893? false
-
-Set version is: [Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1870]
-
-Nikita Khrushchev was born in the year 1894.
-
-Hash code for map "birthYears" is = 2074808840
-
-Is the map empty? false
-
-List of Soviet Leaders: [Josef Stalin, Leonid Brezhnev, Mikhail Gorbachev, Konstantin Chernenko, Nikita Khrushchev, Yuri Andropov, Vladimir Lenin]
-
-{Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1870}
-
-[1878, 1906, 1931, 1911, 1894, 1914, 1870]
-
-Are the maps "birthYears" and "b" equal to eachother? true
-
-1881
-
-"birthYears" after getOrDefault method: {Josef Stalin=1878, Leonid Brezhnev=1906, Mikhail Gorbachev=1931, Konstantin Chernenko=1911, Nikita Khrushchev=1894, Yuri Andropov=1914, Vladimir Lenin=1870}
-*/
