d6b648669891cbd9d89eb1c2adf1e69c52f46091
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-03-12 14:53:25 -0400

    Add files via upload

--- OrganizingData/Queuer.java
+++ OrganizingData/Queuer.java
@@ -17,13 +17,27 @@ public class Queuer {
 		countries.remove("France");
 
 		System.out.println(countries);
+
+		System.out.println(countries.peek()); // peeks at head with no exception for empty queues
+      
+      System.out.println(countries);
+      
+      countries.add("France");
+      
+      System.out.println(countries);
+      
+      countries.poll(); // no exception for empty queues
+      
+      System.out.println(countries);
+      
+      countries.add("France");
+      
+      System.out.println(countries);
+      
+      countries.remove(); // exception for empty queues
+      
+      System.out.println(countries);
+      
+      System.out.println(countries.element()); // peeks at head but there is exception for empty queues
 	}
 }
-
-/*
-Output:
-[France]
-Without violating the capacity restriction: [France, Sweden]
-[Sweden]
-
-*/
