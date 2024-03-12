ddc3138e986b85e0d27862d2ac04b3d34c7a41f3
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-03-12 14:54:47 -0400

    Update Queuer.java

--- OrganizingData/Queuer.java
+++ OrganizingData/Queuer.java
@@ -20,24 +20,38 @@ public class Queuer {
 
 		System.out.println(countries.peek()); // peeks at head with no exception for empty queues
       
-      System.out.println(countries);
+      		System.out.println(countries);
       
-      countries.add("France");
+      		countries.add("France");
       
-      System.out.println(countries);
+     		System.out.println(countries);
       
-      countries.poll(); // no exception for empty queues
+      		countries.poll(); // no exception for empty queues
       
-      System.out.println(countries);
+      		System.out.println(countries);
       
-      countries.add("France");
+      		countries.add("France");
       
-      System.out.println(countries);
+      		System.out.println(countries);
       
-      countries.remove(); // exception for empty queues
+      		countries.remove(); // exception for empty queues
       
-      System.out.println(countries);
+      		System.out.println(countries);
       
-      System.out.println(countries.element()); // peeks at head but there is exception for empty queues
+      		System.out.println(countries.element()); // peeks at head but there is exception for empty queues
 	}
 }
+
+/*
+Output:
+[France]
+Without violating the capacity restriction: [France, Sweden]
+[Sweden]
+Sweden
+[Sweden]
+[France, Sweden]
+[Sweden]
+[France, Sweden]
+[Sweden]
+
+*/
