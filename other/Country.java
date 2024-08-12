f8870aaa1035876ab217014df27eef267cf9e842
Akhil B <akhilb1644@gmail.com>
2024-08-11 20:51:07 -0400

    Orignal edits

---
+++ other/Country.java
@@ -0,0 +1,35 @@
+public class Country {
+	private int population;
+	private String name;
+	private Continent continent;
+	
+	public Country(int population, String name,Continent continent) {
+		this.population = population;
+		this.name = name;
+		this.continent = continent;
+	}
+	
+	public int getPopulaion() {
+		return population;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Continent getContinent() {
+		return continent;
+	}
+	
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setContinent(Continent continent) {
+		this.continent = continent;
+	}
+}
