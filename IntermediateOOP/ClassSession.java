3f08ef3457502e9c9bff8c208442d11cb209a78f
Akhil B <48994987+akhilb1644@users.noreply.github.com>
2024-08-22 14:11:44 -0400

    Add files via upload

--- IntermediateOOP/ClassSession.java
+++ IntermediateOOP/ClassSession.java
@@ -1,5 +1,7 @@
 package IntermediateOOP;
 
+import java.util.Scanner;
+
 public class ClassSession extends Session {
 	protected String leader; // could be teacher of professor
 	protected EduSetting setting;
@@ -9,6 +11,13 @@ public class ClassSession extends Session {
 		this.topic = topic;
 		this.leader= leader;
 		this.setting = setting;
+	}
+	
+	public ClassSession() {
+		this.population = 0;
+		this.topic = "";
+		this.leader = "";
+		this.setting = EduSetting.OTHER_SCHOOL;
 	}
 	
 	final String getLeader() {
@@ -25,5 +34,31 @@ public class ClassSession extends Session {
 	
 	final void setSetting(EduSetting setting) {
 		this.setting = setting;
+	}
+	
+	protected static Session makeObject() {
+		Scanner k = new Scanner(System.in);
+		ClassSession cs = new ClassSession();
+		String s;
+		int i;
+		
+		System.out.print("What is the population of this session? ");
+		i = k.nextInt();
+		cs.setPopulation(i);
+		
+		System.out.print("What is the topic of this session ");
+		s = k.nextLine();
+		cs.setTopic(s);
+		
+		k.nextLine();
+		
+		System.out.print("Who is the leader of this session? ");
+		s = k.nextLine();
+		cs.setLeader(s);
+		
+		System.out.print("What is the setting of the session, pick from the below options? ");
+		// Add options below based off of enum
+		
+		return cs;
 	}
 }
---
+++ IntermediateOOP/Main.java
@@ -0,0 +1,10 @@
+package IntermediateOOP;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
